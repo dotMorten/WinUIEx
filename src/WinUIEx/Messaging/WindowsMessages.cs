c274859581793b1a5cc4756ee909da5b6d7b5e09
Morten Nielsen <1378165+dotMorten@users.noreply.github.com>
2022-06-11 08:40:51 -0700

    Added more enums to messages list

--- src/WinUIEx/Messaging/WindowsMessages.cs
+++ src/WinUIEx/Messaging/WindowsMessages.cs
@@ -25,10 +25,10 @@
 
 // ReSharper disable InconsistentNaming
 
-using System.Diagnostics.CodeAnalysis;
-
 namespace WinUIEx.Messaging
 {
+    // Good list of message numbers: https://wiki.winehq.org/List_Of_Windows_Messages
+
     /// <summary>
     /// This enum defines the windows messages we respond to.
     /// See more on Windows messages <a href="https://docs.microsoft.com/en-us/windows/win32/learnwin32/window-messages">here</a>
@@ -40,41 +40,142 @@ namespace WinUIEx.Messaging
         /// See <a href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-move">WM_MOVE message</a>.
         /// </summary>
         WM_MOVE = 3,
+
+        /// <summary>
+        /// Sent as a signal that a window or an application should terminate.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-close">Microsoft Docs</seealso>
+        WM_CLOSE = 0x0010,
+
+        /// <summary>
+        /// A window receives this message when the user chooses a command from the Window menu (formerly known as the
+        /// system or control menu) or when the user chooses the maximize button, minimize button, restore button, or 
+        /// close button.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/menurc/wm-syscommand">Microsoft Docs</seealso>
+        WM_SYSCOMMAND = 0x0112,
+
+        /// <summary>
+        /// Sent to a window if the mouse causes the cursor to move within a window and mouse input is not captured.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/menurc/wm-setcursor">Microsoft Docs</seealso>
+        WM_SETCURSOR = 0x20,
+
+        /// <summary>
+        /// Posted to a window when the cursor is moved within the nonclient area of the window. This message is posted
+        /// to the window that contains the cursor. If a window has captured the mouse, this message is not posted.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-ncmousemove">Microsoft Docs</seealso>
+        WM_NCMOUSEMOVE = 0x00a0,
+
+        /// <summary>
+        /// Sent to both the window being activated and the window being deactivated. If the windows use the same input queue,
+        /// the message is sent synchronously, first to the window procedure of the top-level window being deactivated, then
+        /// to the window procedure of the top-level window being activated. If the windows use different input queues, the
+        /// message is sent asynchronously, so the window is activated immediately.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-activate">Microsoft Docs</seealso>
+        WM_ACTIVATE = 0x0006,
+
+        /// <summary>
+        /// Sent when a window belonging to a different application than the active window is about to be activated. The
+        /// message is sent to the application whose window is being activated and to the application whose window is 
+        /// being deactivated.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-activateapp">Microsoft Docs</seealso>
+        WM_ACTIVATEAPP = 0x001c,
+
+        /// <summary>
+        /// Sent to a window whose size, position, or place in the Z order is about to change as a result of a call to
+        /// the SetWindowPos function or another window-management function.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-windowposchanging">Microsoft Docs</seealso>
+        WM_WINDOWPOSCHANGING = 0x0046,
+
+        /// <summary>
+        /// Sent to a window whose size, position, or place in the Z order has changed as a result of a call to the SetWindowPos function or another window-management function.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-windowposchanged">Microsoft Docs</seealso>
+        WM_WINDOWPOSCHANGED = 0x0047,
+
+        /// <summary>
+        /// Sets the text of a window.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-settext">Microsoft Docs</seealso>
+        WM_SETTEXT = 0x000c,
+
+        /// <summary>
+        /// Copies the text that corresponds to a window into a buffer provided by the caller.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-gettext">Microsoft Docs</seealso>
+        WM_GETTEXT = 0x000d,
+
+        /// <summary>
+        /// Determines the length, in characters, of the text associated with a window.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-gettextlength">Microsoft Docs</seealso>
+        WM_GETTEXTLENGTH = 0x000e,
+
+        /// <summary>
+        /// Sent to a window when its nonclient area needs to be changed to indicate an active or inactive state.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-ncactivate">Microsoft Docs</seealso>
+        WM_NCACTIVATE = 0x0086,
+
+        /// <summary>
+        /// Sent to the window that is losing the mouse capture.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-capturechanged">Microsoft Docs</seealso>
+        WM_CAPTURECHANGED = 0x0215,
+
+        /// <summary>
+        /// Posted to a window when the cursor leaves the nonclient area of the window specified in a prior call to TrackMouseEvent.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-ncmouseleave">Microsoft Docs</seealso>
+        WM_NCMOUSELEAVE = 0x02a2,
+
         /// <summary>
         /// Sent after a window has been moved.
         /// See <a href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-moving">WM_MOVING message</a>.
         /// </summary>
         WM_MOVING = 0x0216,
+
         /// <summary>
         /// Sent to a window after its size has changed.
         /// See <a href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-size">WM_SIZE message</a>.
         /// </summary>
         WM_SIZE = 4,
+
         /// <summary>
         /// Sent to a window that the user is resizing. By processing this message, an application can monitor the size and position of the drag rectangle and, if needed, change its size or position.
         /// See <a href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-sizing">WM_SIZING message</a>.
         /// </summary>
         WM_SIZING = 0x0214,
+
         /// <summary>
         /// Sent to a window when the size or position of the window is about to change. An application can use this message to override the window's default maximized size and position, or its default minimum or maximum tracking size.
         /// See <a href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-getminmaxinfo">WM_GETMINMAXINFO message</a>.
         /// </summary>
         WM_GETMINMAXINFO = 0x0024,
+
         /// <summary>
         /// Sent when an application changes the enabled state of a window. It is sent to the window whose enabled state is changing. This message is sent before the <a href="https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-enablewindow">EnableWindow</a> function returns, but after the enabled state (WS_DISABLED style bit) of the window has changed.
         /// See <a href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-enable">WM_ENABLE message</a>.
         /// </summary>
         WM_ENABLE = 0x000A,
+
         /// <summary>
         /// Sent one time to a window after it enters the moving or sizing modal loop. The window enters the moving or sizing modal loop when the user clicks the window's title bar or sizing border, or when the window passes the WM_SYSCOMMAND message to the DefWindowProc function and the wParam parameter of the message specifies the SC_MOVE or SC_SIZE value. 
         /// See <a href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-entersizemove">WM_ENTERSIZEMOVE message</a>.
         /// </summary>
         WM_ENTERSIZEMOVE = 0x0231,
+
         /// <summary>
         /// ent one time to a window, after it has exited the moving or sizing modal loop. The window enters the moving or sizing modal loop when the user clicks the window's title bar or sizing border, or when the window passes the WM_SYSCOMMAND message to the DefWindowProc function and the wParam parameter of the message specifies the SC_MOVE or SC_SIZE value.
         /// See <a href="https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-exitsizemove">WM_EXITSIZEMOVE message</a>.
         /// </summary>
         WM_EXITSIZEMOVE = 0x0232,
+
         /// <summary>
         /// Notifies a window that the user clicked the right mouse button (right-clicked) in the window.
         /// See <a href="https://docs.microsoft.com/en-us/windows/win32/menurc/wm-contextmenu">WM_CONTEXTMENU message</a>
@@ -179,6 +280,8 @@ namespace WinUIEx.Messaging
         /// Used to define private messages for use by private window classes, usually of the form WM_USER+x, where x is an integer value.
         /// </summary>
         WM_USER = 0x0400,
+
+        WM_GETICON = 0x007f,
 
         /// <summary>
         /// Associates a new large or small icon with a window. The system displays the large icon in the ALT+TAB dialog box, and the small icon in the window caption.

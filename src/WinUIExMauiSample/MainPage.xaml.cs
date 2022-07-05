8813c2f58dd831f92d4bf59d69fb38b530707438
Morten <mnielsen@esri.com>
2022-07-05 10:00:52 -0700

    Improve maui sample

--- src/WinUIExMauiSample/MainPage.xaml.cs
+++ src/WinUIExMauiSample/MainPage.xaml.cs
@@ -9,10 +9,15 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnFullScreenClicked(object sender, EventArgs e)
         {
-            Navigation.PushModalAsync(new Page());
-            SemanticScreenReader.Announce(CounterBtn.Text);
+#if WINDOWS
+            var manager = WinUIEx.WindowManager.Get(this.Window.Handler.PlatformView as Microsoft.UI.Xaml.Window);
+            if (manager.PresenterKind == Microsoft.UI.Windowing.AppWindowPresenterKind.Overlapped)
+                manager.PresenterKind = Microsoft.UI.Windowing.AppWindowPresenterKind.FullScreen;
+            else
+                manager.PresenterKind = Microsoft.UI.Windowing.AppWindowPresenterKind.Overlapped;
+#endif
         }
     }
 }

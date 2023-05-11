6ba3989e06640ee991440e12dadeba2b20038f9e
Morten Nielsen <mort5161@esri.com>
2023-05-11 16:30:56 -0700

    doc

--- src/WinUIEx/MonitorInfo.cs
+++ src/WinUIEx/MonitorInfo.cs
@@ -54,15 +54,19 @@ namespace WinUIEx
         public string Name { get; }
 
         /// <summary>
-        /// Gets the display monitor rectangle, expressed in virtual-screen coordinates. Note that if the monitor is not the primary
-        /// display monitor, some of the rectangle's coordinates may be negative values.
+        /// Gets the display monitor rectangle, expressed in virtual-screen coordinates.
         /// </summary>
+        /// <remarks>
+        /// <note>If the monitor is not the primary display monitor, some of the rectangle's coordinates may be negative values.</note>
+        /// </remarks>
         public Rect RectMonitor { get; }
 
         /// <summary>
-        /// Gets the work area rectangle of the display monitor, expressed in virtual-screen coordinates. Note that if the monitor is 
-        /// not the primary display monitor, some of the rectangle's coordinates may be negative values.
+        /// Gets the work area rectangle of the display monitor, expressed in virtual-screen coordinates.
         /// </summary>
+        /// <remarks>
+        /// <note>If the monitor is not the primary display monitor, some of the rectangle's coordinates may be negative values.</note>
+        /// </remarks>
         public Rect RectWork { get; }
 
         /// <inheritdoc />

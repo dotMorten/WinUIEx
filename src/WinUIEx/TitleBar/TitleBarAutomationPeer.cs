2559c5e833d6872970cf7791e1cf0e8d35c153cd
Morten Nielsen <mort5161@esri.com>
2024-09-11 12:19:57 -0700

    Added TitleBar control

---
+++ src/WinUIEx/TitleBar/TitleBarAutomationPeer.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.UI.Xaml.Automation.Peers;
+
+namespace WinUIEx;
+
+/// <summary>
+/// Automation peer for the <see cref="TitleBar"/> control.
+/// </summary>
+public class TitleBarAutomationPeer : FrameworkElementAutomationPeer
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TitleBar"/> class.
+    /// </summary>
+    /// <param name="owner">TitleBar owner</param>
+    public TitleBarAutomationPeer(TitleBar owner) : base(owner)
+    {
+    }
+
+    /// <inheritdoc />
+    protected override AutomationControlType GetAutomationControlTypeCore()
+    {
+        return base.GetAutomationControlTypeCore();
+    }
+
+    /// <inheritdoc />
+    protected override string GetClassNameCore()
+    {
+        return nameof(TitleBar);
+    }
+
+    /// <inheritdoc />
+    protected override string GetNameCore()
+    {
+        var name = base.GetNameCore();
+        if (string.IsNullOrEmpty(name))
+        {
+            if (Owner is TitleBar titleBar)
+            {
+                name = titleBar.Name;
+            }
+        }
+        return name;
+    }
+}
---
+++ src/WinUIEx/TitleBar/TitleBarTemplateSettings.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace WinUIEx;
+
+/// <summary>
+/// Class used to forward the IconElement property to the template.
+/// </summary>
+public class TitleBarTemplateSettings : DependencyObject
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TitleBar"/> class.
+    /// </summary>
+    public TitleBarTemplateSettings()
+    {
+    }
+
+    /// <summary>
+    /// Gets or sets the IconElement property
+    /// </summary>
+    public IconElement? IconElement
+    {
+        get { return (IconElement?)GetValue(IconElementProperty); }
+        set { SetValue(IconElementProperty, value); }
+    }
+
+    /// <summary>Identifies the <see cref="IconElement"/> dependency property.</summary>
+    public static readonly DependencyProperty IconElementProperty =
+    DependencyProperty.Register("IconElement", typeof(IconElement), typeof(TitleBarTemplateSettings), new PropertyMetadata(null));
+}

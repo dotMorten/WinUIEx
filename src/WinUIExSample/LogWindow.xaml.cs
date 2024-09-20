71259842ca3cd1302978461fe8b4e9a9402c1faa
dotMorten <mn@iter.dk>
2024-09-20 13:12:53 -0700

    Fix sample build errors

--- src/WinUIExSample/LogWindow.xaml.cs
+++ src/WinUIExSample/LogWindow.xaml.cs
@@ -1,4 +1,7 @@
-namespace WinUIExSample;
+using System.Linq;
+using Microsoft.UI.Xaml;
+
+namespace WinUIExSample
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.

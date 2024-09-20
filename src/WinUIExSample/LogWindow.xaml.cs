0b9a0d4b1d642c17d68ef30c7810ebf5db1fbe66
dotMorten <mn@iter.dk>
2024-09-20 13:07:40 -0700

    Ensure event log is updated on the UI thread
    Fixes #169. Thank you @gmgallo

--- src/WinUIExSample/LogWindow.xaml.cs
+++ src/WinUIExSample/LogWindow.xaml.cs
@@ -1,23 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using WinUIEx;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace WinUIExSample
+namespace WinUIExSample;
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -37,7 +18,13 @@ namespace WinUIExSample
         {
             var log = MainWindow.WindowEvents;
             if (log != null)
-                WindowEventLog.Text = string.Join('\n', log.Reverse());
+            {
+                var multiline_text = string.Join('\n', log.Reverse());
+                DispatcherQueue.TryEnqueue(() =>
+                {
+                    WindowEventLog.Text = multiline_text;
+                });
+            }
         }
     }
 }

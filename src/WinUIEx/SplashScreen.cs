b961ccda7e74a5c166a541384bc0ec041e290f9b
dotMorten <mn@iter.dk>
2025-09-12 14:30:48 -0700

    Fix nullability error

--- src/WinUIEx/SplashScreen.cs
+++ src/WinUIEx/SplashScreen.cs
@@ -41,7 +41,9 @@ namespace WinUIEx
             _getWindow = () => (Window)Activator.CreateInstance(window)!;
         }
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
         private SplashScreen()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
         {
             this.Activated += SplashScreen_Activated;
             this.DispatcherQueue.TryEnqueue(DispatcherQueuePriority.Normal, () =>

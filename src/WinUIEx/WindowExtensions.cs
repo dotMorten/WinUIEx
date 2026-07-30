fb2d9a263cb295998d42e6fdd452f181674cbcfa
Renn <8340896+AkazaRenn@users.noreply.github.com>
2026-07-30 17:05:38 -0400

    Fix xml doc

--- src/WinUIEx/WindowExtensions.cs
+++ src/WinUIEx/WindowExtensions.cs
@@ -396,8 +396,7 @@ namespace WinUIEx
         public static DataTransferManager GetDataTransferManagerForWindow(this Microsoft.UI.Xaml.Window window) => HwndExtensions.GetDataTransferManagerForWindow(window.GetWindowHandle());
 
         /// <summary>
-        /// Displays the Windows Share UI for this window and supplies the specified
-        /// <see cref="DataPackage"/> when the user completes the share operation.
+        /// Displays the Windows Share UI for this window with the specified <see cref="DataPackage"/> .
         /// </summary>
         /// <param name="window">The window for which to display the Share UI.</param>
         /// <param name="data">The data package to share.</param>

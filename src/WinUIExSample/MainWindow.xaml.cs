32a62fc74c2513b7c81870b30fcad5cd51dd8f0f
Renn <8340896+AkazaRenn@users.noreply.github.com>
2026-08-02 22:47:15 -0400

    Revert some changes that should no tbe there

--- src/WinUIExSample/MainWindow.xaml.cs
+++ src/WinUIExSample/MainWindow.xaml.cs
@@ -11,7 +11,6 @@ using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
-using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Graphics;
 using WinUIEx;
